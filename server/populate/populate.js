c69b9fd819f286fe0b25f2634867bec461df05ed
alexsincai <alex.sincai@yahoo.co.uk>
2023-02-13 10:12:39 +0200

    color population function

--- server/populate/populate.js
+++ server/populate/populate.js
@@ -6,40 +6,44 @@ const mongoose = require("mongoose");
 const names = require("./names.json");
 const levels = require("./levels.json");
 const positions = require("./positions.json");
-//const colors = require("./colors.json")
+const colors = require("./colors.json");
 const EmployeeModel = require("../db/employee.model");
-const colorModel = require("../db/colorModel.model")
+const colorModel = require("../db/colorModel.model");
 // const fructModel = require("../db/fruct.model")
 // const fructe = require("./fructe.json");
 const mongoUrl = process.env.MONGO_URL;
 
 if (!mongoUrl) {
-  console.error("Missing MONGO_URL environment variable");
-  process.exit(1); // exit the current program
+    console.error("Missing MONGO_URL environment variable");
+    process.exit(1); // exit the current program
 }
 
 const pick = (from) => from[Math.floor(Math.random() * (from.length - 0))];
 
 const populateEmployees = async () => {
-  await EmployeeModel.deleteMany({});
-  const colors = (await colorModel.find({}).lean())
-  console.log(colors);
-  const employees = names.map((name) => ({
-    name,
-    level: pick(levels),
-    position: pick(positions),
-    colors: pick(colors.map((c) => c._id)),
-  }));
+    await EmployeeModel.deleteMany({});
+    await colorModel.deleteMany({});
 
-  await EmployeeModel.create(...employees);
-  console.log("Employees created");
+    await colorModel.create(...colors.map((c) => ({ name: c })));
+
+    const culori = await colorModel.find({}).lean();
+
+    const employees = names.map((name) => ({
+        name,
+        level: pick(levels),
+        position: pick(positions),
+        colors: pick(culori.map((c) => c._id)),
+    }));
+
+    await EmployeeModel.create(...employees);
+    console.log("Employees created");
 };
 // const populateFructe = async () => {
 //   await fructModel.deleteMany({});
 
 //   const items = fructe.map((name) => ({
 //     name
-   
+
 //   }));
 
 //   await fructModel.create(...items);
@@ -47,16 +51,16 @@ const populateEmployees = async () => {
 // };
 
 const main = async () => {
-  await mongoose.connect(mongoUrl);
+    await mongoose.connect(mongoUrl);
 
-  await populateEmployees();
+    await populateEmployees();
 
-  // await populateFructe();
+    // await populateFructe();
 
-  await mongoose.disconnect();
+    await mongoose.disconnect();
 };
 
 main().catch((error) => {
-  console.error(error);
-  process.exit(1);
+    console.error(error);
+    process.exit(1);
 });
